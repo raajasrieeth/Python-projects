697f63d361df96b581ccc857755bb51011f33641
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-09-21 11:45:53 +0530

    Add files via upload

--- work_withPDf.py
+++ work_withPDf.py
@@ -1,24 +1,32 @@
 import PyPDF2
+import Speaker 
+import time
 
-filename = str(input("Enter the filename"))
+filename = str(input("Enter the filename\t"))
 path = "D:\\python_envs\\All_other_codes\\" + filename+'.pdf'# can be changed  
 file = PyPDF2.PdfFileReader(path)
+try: 
+	print("The book" , filename, "was authored by" , file.getDocumentInfo()['/Creator'] , "and is " , file.getNumPages() , "pages long")# print the author name
+	firstpage , lastpage = int(input("Enter the first page\t")) , int(input('Enter the last page\t'))
+	text = " "
 
-print("The book" , filename, "was authored by" , file.getDocumentInfo()['/Author'] , "and is " , file.getNumPages() , "pages long")# print the author name
-
-
-firstpage , lastpage = int(input("Enter the first page")) , int(input('Enter the last page'))
-text = " "
-
-for i in range(firstpage, lastpage+1):#iterate over the pages
-	text+= file.getPage(i).extractText() # so that all the content in the pages is stored in a global variable
+	for i in range(4 , 7):#iterate over the pages
+		text += file.getPage(i).extractText() # so that all the content in the pages is stored in a global variable
 
 
-with open('Contents.txt' , 'w') as f:
-	f.write(text)#dump the contents in the range of pages in a *.txt file
+	with open('Contents.txt' , 'w' , encoding="utf-8") as f:
+		f.write(text)#dump the contents in the range of pages in a *.txt file
+	print(text)
+
+	time.sleep(1)
+	Speaker.speak("Reading out the contents.")
+	time.sleep(2)
+	Speaker.speak(text)
+
+except:
+	print("error")
 
 
 
-print(text)#reference
 
 
