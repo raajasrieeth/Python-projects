aa6b2a707e43e142172a88c3ee1d4d71081f2d36
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-09-22 13:51:58 +0530

    Update Speaker.py

--- Speaker.py
+++ Speaker.py
@@ -1,9 +1,11 @@
 import pyttsx3 # to be pip installed
+# uses the default voices available in your os . The speed and voice settings can be changed from the control panel in Windows.
 
 engine = pyttsx3.init('sapi5')#initialize the engine
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
 def speak(audio):
+    '''used for speaking a string given as a parameter.'''
     engine.say(audio)
     engine.runAndWait()
