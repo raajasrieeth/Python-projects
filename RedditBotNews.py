99170e5f3e7191724517827b1890024df37344ad
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-10-19 11:15:38 +0530

    Update RedditBotNews.py

--- RedditBotNews.py
+++ RedditBotNews.py
@@ -1,11 +1,14 @@
+# get top posts in a subreddit
 import praw # pip install this
 
-reddit = praw.Reddit(
+reddit = praw.Reddit(   # my application
     client_id='UctJAJCE-8-nvA',
     client_secret='1FsIcCXLLrKqxcAU3bRISfzuUrI',
     user_agent = 'my user agent'
 
 )
-for submission in reddit.subreddit('news').hot(limit=5):
+sb = input("Enter subreddit to go to")
+n = int(input("Enter the number of titles needed"))
+for submission in reddit.subreddit(sb).hot(limit=n):
     print(submission.title)
     print('=========================')
