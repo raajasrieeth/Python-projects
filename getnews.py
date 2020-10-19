47ea5c31c8789b818fe6cac3f6f9139a5237164d
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-10-19 11:09:44 +0530

    Add files via upload

---
+++ getnews.py
@@ -0,0 +1,11 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id='UctJAJCE-8-nvA',
+    client_secret='1FsIcCXLLrKqxcAU3bRISfzuUrI',
+    user_agent = 'my user agent'
+
+)
+for submission in reddit.subreddit('news').hot(limit=5):
+    print(submission.title)
+    print('=========================')
